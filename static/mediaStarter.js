f63520a9ff871cb690cca1623d1c5e235c5e163d
¨Hugo <hugomecalcoeliosa@MacBook-Air-de-Hugo.local>
2013-05-08 06:27:56 -0500

    static images added all

--- static/mediaStarter.js
+++ static/mediaStarter.js
@@ -227,7 +227,7 @@ function createOverlays() {
         {'scale':
         {'magnitude': 0.5,
         'reference': gapi.hangout.av.effects.ScaleReference.WIDTH}});
-      overlays['star'].setPosition(-0.3, 0.3);
+      overlays['star'].setPosition(-0.3, 0.4);
       minScale['star'] = 1.5;
       maxScale['star'] = 2.5;
 
